beeade08b83729358a012848042039ad8750c627
Eliel Oshiokameh <95494441+elieloshiokameh@users.noreply.github.com>
2023-12-15 04:41:24 +0000

    changes to home page and layout

---
+++ src/components/Home/index.js
@@ -0,0 +1,38 @@
+import Typewriter from 'typewriter-effect'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import PortfolioImage from '../../assets/images/portfolioImageModified.png'
+
+const Home = () => {
+
+
+    return (
+        <div className="container home-page">
+            <div className="introduction" >
+                <h1>Hi, I am
+                    <br />
+                    Eliel Oshiokameh
+                    <br />
+                </h1>
+                <img src={PortfolioImage} alt='portfoliopic' />
+    
+            </div>
+
+            <h2>
+                I am a
+                <Typewriter
+                    options={{
+                        strings: ['Computer Science Student', 'Software Engineer', 'Front/BackEnd Developer'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </h2>
+            <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+
+
+        </div>
+    );
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,30 +1,64 @@
 import './index.scss'
-import {Link, NavLink} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoE from '../../assets/images/logo-e.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () =>(
+const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoE} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="eliel" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink
+             exact="true" 
+             activeclassname="active" 
+             to="/"
+            >
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
 
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+
+            <NavLink
+             exact="true" 
+             activeclassname="active" 
+             className="about-link" 
+             to="/about"
+            >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
 
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+
+            <NavLink
+             exact="true" 
+             activeclassname="active" 
+             className="contact-link" 
+             to="/contact"
+            >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
 
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/eliel-oshiokameh/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.github.com/eliel-oshiokameh/'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.instagram.com/eliel.oshiokameh/'>
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
 
     </div>
 )
