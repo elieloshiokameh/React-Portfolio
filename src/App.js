774630eb48666b83248451bd059c27b4b73cc99d
Eliel Oshiokameh <elieloshiokameh@Eliels-MacBook-Pro-2.local>
2025-05-09 19:33:06 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About';
@@ -13,10 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
-          <Route path='/blog' element={<Blog />}></Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
         </Route>
       </Routes>
     </ThemeProvider>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import '../../styles.scss'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import LogoE from '../../assets/images/logo-e.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,13 +9,12 @@ import { ThemeContext } from '../../context/ThemeContext'
 
 const Sidebar = () => {
     const [showNav, setShowNav] = useState(false);
-    const [showDropdown, setShowDropdown] = useState(false);
     const { isDarkMode, toggleTheme } = useContext(ThemeContext);
-    const dropdownRef = useRef(null);
+    const navRef = useRef(null);
 
     const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-            setShowDropdown(false);
+        if (navRef.current && !navRef.current.contains(event.target)) {
+            setShowNav(false);
         }
     };
 
@@ -27,7 +26,6 @@ const Sidebar = () => {
     }, []);
 
     const handleLinkClick = () => {
-        setShowDropdown(false);
         setShowNav(false);
     };
 
@@ -37,96 +35,29 @@ const Sidebar = () => {
                 <img src={LogoE} alt="logo" />
                 <img className="sub-logo" src={LogoSubtitle} alt="eliel" />
             </Link>
-            <nav className={showNav ? 'mobile-show' : ''} ref={dropdownRef}>
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    to="/"
-                    onClick={handleLinkClick}
-                >
+            <nav className={showNav ? 'mobile-show' : ''} ref={navRef}>
+                <Link to="/" className="nav-link" onClick={handleLinkClick}>
                     Home
-                </NavLink>
+                </Link>
 
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="about-link"
-                    to="/about"
-                    onClick={handleLinkClick}
-                >
+                <Link to="/about" className="nav-link" onClick={handleLinkClick}>
                     About
-                </NavLink>
+                </Link>
 
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="blog-link"
-                    to="/blog"
-                    onClick={handleLinkClick}
-                >
+                <Link to="/blog" className="nav-link" onClick={handleLinkClick}>
                     Blog
-                </NavLink>
+                </Link>
 
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="contact-link"
-                    to="/contact"
-                    onClick={handleLinkClick}
-                >
+                <Link to="/contact" className="nav-link" onClick={handleLinkClick}>
                     Contact
-                </NavLink>
+                </Link>
 
                 <div className="theme-toggle" onClick={toggleTheme}>
                     <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} color="#4d4d4e" />
                 </div>
             </nav>
-            <div className="hamburger-menu" onClick={() => setShowDropdown(!showDropdown)}>
-                <FontAwesomeIcon icon={showDropdown ? faClose : faBars} color="#4d4d4e" />
-            </div>
-            <div className={`dropdown-menu ${showDropdown ? 'show' : ''}`}>
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    to="/"
-                    onClick={handleLinkClick}
-                >
-                    Home
-                </NavLink>
-
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="about-link"
-                    to="/about"
-                    onClick={handleLinkClick}
-                >
-                    About
-                </NavLink>
-
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="blog-link"
-                    to="/blog"
-                    onClick={handleLinkClick}
-                >
-                    Blog
-                </NavLink>
-
-                <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="contact-link"
-                    to="/contact"
-                    onClick={handleLinkClick}
-                >
-                    Contact
-                </NavLink>
-
-                <div className="theme-toggle" onClick={toggleTheme}>
-                    <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} color="#4d4d4e" />
-                </div>
+            <div className="hamburger-menu" onClick={() => setShowNav(!showNav)}>
+                <FontAwesomeIcon icon={showNav ? faClose : faBars} color="#4d4d4e" />
             </div>
         </div>
     )
